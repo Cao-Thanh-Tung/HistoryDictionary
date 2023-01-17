f0c8f235e7318d0419bd7f5099d1ca402c7e43e3
Dung <Dung@192.168.1.14>
2023-01-17 23:21:10 +0700

    create searchbar

---
+++ src/main/java/frontend/view/Searchbar.java
@@ -0,0 +1,29 @@
+package frontend.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Searchbar extends Application {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch();
+	}
+
+	@Override
+	public void start(Stage state) throws Exception {
+		// TODO Auto-generated method stub
+		Parent root = FXMLLoader.load(getClass().getResource("/frontend/model/searchbar.fxml"));
+		Scene scene = new Scene(root);
+		
+		state.setTitle("Searchbar");
+		state.setScene(scene);
+		state.show();
+	}
+
+}
