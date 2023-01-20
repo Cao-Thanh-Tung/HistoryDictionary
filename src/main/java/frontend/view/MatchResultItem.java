5756bed472573fb80674dea8e69eb3130948b752
Cao Thanh Tung <caothanhtung08082002@gmail.com>
2023-01-20 16:53:53 +0700

    matchedResultItem completed

--- src/main/java/frontend/view/MatchResultItem.java
+++ src/main/java/frontend/view/MatchResultItem.java
@@ -1,5 +1,25 @@
 package frontend.view;
 
-public class MatchResultItem {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class MatchResultItem extends Application {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch();
+	}
+
+	@Override
+	public void start(Stage state) throws Exception {
+		// TODO Auto-generated method stub
+		Parent root = FXMLLoader.load(getClass().getResource("/frontend/model/matchResultItem.fxml"));
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("/frontend/style/matchResultItem.css").toExternalForm());
+		state.setTitle("Searchbar");
+		state.setScene(scene);
+		state.show();
+	}
 }
